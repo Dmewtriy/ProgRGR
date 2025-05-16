342e39eaccdfd6c0f9695d104198dfcdf9f8ed3b
Tair Olegovich <gidrolaz.tr@gmail.com>
2025-05-16 17:48:10 +0700

    FileController. GetDataHex и GetDataBinary - методы, которые конвертируют данные под форму

--- ProgRGR/FileController.cs
+++ ProgRGR/FileController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BinaryViewer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,55 @@ namespace ProgRGR
 {
     internal class FileController
     {
+        private FileManager fileManager; 
 
+        public FileController(string path)
+        {
+            fileManager = new FileManager();
+            fileManager.Open(path);
+        }
+
+        public string[] GetDataHex()
+        {
+            byte[] data = fileManager.GetNewPage();
+
+            string[] hex = new string[data.Length + (int) Math.Ceiling(data.Length / 16.0)];
+
+            for (int i = 0; i < hex.Length; i++)
+            {
+                if (i % 17 == 0)
+                {
+                    hex[i] = Convert.ToString(i / 17, 16).PadLeft(10, '0').ToUpper() + "0";
+                    continue;
+                }
+                hex[i] = Convert.ToString(data[i], 16).PadLeft(2, '0').ToUpper();
+            }
+
+            return hex;
+        }
+
+        public string[] GetDataBinary()
+        {
+            byte[] data = fileManager.GetNewPage();
+
+            string[] binary = new string[data.Length + (int)Math.Ceiling(data.Length / 16.0)];
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (i % 17 == 0)
+                {
+                    binary[i] = Convert.ToString(i / 17, 16).PadLeft(10, '0').ToUpper() + "0";
+                    continue;
+                }
+                binary[i] = Convert.ToString(data[i], 2).PadLeft(8, '0').ToUpper();
+            }
+
+            return binary;
+        }
+
+        public void Close()
+        {
+            fileManager.CloseFile();
+        }
     }
 }
