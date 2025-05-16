a782d67a2226d1f5f6ba26f665f154ccde21361a
Tair Olegovich <gidrolaz.tr@gmail.com>
2025-05-16 17:00:36 +0700

    Файловый менеджер и страницы, из которых состоит буффер

---
+++ BinaryViewer/Page.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryViewer
+{
+    public class Page
+    {
+        private byte[] data;
+        private long index;
+
+        public Page(byte[] bytes, long index) 
+        {
+            data = bytes;
+            this.index = index;
+        }
+
+        public long Index
+        {
+            get { return index; }
+            private set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                else
+                {
+                    index = value;
+                }
+            }
+        }
+
+        public byte[] Data
+        {
+            get { return data; }
+        }
+    }
+}
--- ProgRGR/FileController.cs
+++ ProgRGR/FileController.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BinaryViewer
+namespace ProgRGR
 {
-    public class Class1
+    internal class FileController
     {
 
     }
