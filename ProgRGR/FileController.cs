c6e1bf8d56ac2ca55b4de65e1bf87c41d1a06049
Dmewtriy <dankin.dm@mail.ru>
2025-05-25 20:59:18 +0700

    Исправлено чтение данных из файла

--- ProgRGR/FileController.cs
+++ ProgRGR/FileController.cs
@@ -1,11 +1,4 @@
 ﻿using BinaryViewer;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics.Metrics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProgRGR
 {
@@ -29,6 +22,7 @@ namespace ProgRGR
     }
     public class FileController
     {
+        public event Action<string> Errors;
         private FileManager fileManager; 
         public FileController()
         {
@@ -40,69 +34,47 @@ namespace ProgRGR
             fileManager.Open(path);
         }
 
-        public List<DataRow> GetDataHex(int row, int column)
+        public List<DataRow> GetDataHex(int row, int column, int numberBase)
         {
             byte[] data = fileManager.GetNewPage();
             List<DataRow> hex = new List<DataRow>();
-            
+            int dataIndex = 0;
+
+            if (data.Length == 0)
+            {
+                return hex;
+            }
+            if (column == -1) row++;
             int countRows = (int) Math.Ceiling(data.Length / 16.0);
             DataRow dataRow;
 
             for (int i = 0; i < countRows; i++)
             {
                 dataRow = new DataRow();
-                dataRow.numberRow = Convert.ToString(i + row, 16).PadLeft(10, '0').ToUpper() + "0";
-                if (i == 0)
-                {
-                    for (int j = column; j < 16; j++)
-                    {
-                        dataRow.data.Add(Convert.ToString(data[j - column], 16).PadLeft(2, '0').ToUpper());
-                    }
-                }
-                else if (i == countRows - 1)
-                {
-                    int remainder = data.Length - i * 16 == 0 ? 16 : data.Length - i * 16;
+                dataRow.numberRow = Convert.ToString(i + row, numberBase).PadLeft(10, '0').ToUpper() + "0";
 
-                    for (int j = 0; j < remainder; j++)
-                    {
-                        dataRow.data.Add(Convert.ToString(data[i * 16 + j], 16).PadLeft(2, '0').ToUpper());
-                    }
-                }
-                else
+                int endIndex = Math.Min(dataIndex + 16, data.Length);
+
+                if (i == 0 && column > 0)
                 {
-                    for (int j = 0; j < 16; j++)
-                    {
-                        dataRow.data.Add(Convert.ToString(data[i * 16 + j - (16 - column == 16 ? 0 : 16 - column)], 16).PadLeft(2, '0').ToUpper());
-                    }
+                    endIndex -= column;
+                    if (endIndex >= data.Length)
+                        continue;
                 }
+
+
+                for (int j = dataIndex; j < endIndex; j++)
+                {
+                    dataRow.data.Add(Convert.ToString(data[j], numberBase).PadLeft(2, '0').ToUpper());
+                    dataIndex++;
+                }
+
                 hex.Add(dataRow);
             }
 
 
             return hex;
         }
-
-        public string[] GetDataBinary()
-        {
-            byte[] data = fileManager.GetNewPage();
-
-            string[] binary = new string[data.Length + (int)Math.Ceiling(data.Length / 16.0)];
-            int counter = 0;
-
-            for (int i = 0; i < binary.Length; i++)
-            {
-                if (i % 17 == 0)
-                {
-                    binary[i] = Convert.ToString(i / 17, 16).PadLeft(10, '0').ToUpper() + "0";
-                    continue;
-                }
-                binary[i] = Convert.ToString(data[counter], 2).PadLeft(8, '0').ToUpper();
-                counter++;
-            }
-
-            return binary;
-        }
-
         public void Close()
         {
             fileManager.CloseFile();
